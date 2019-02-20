6b1212512213250e947c7ed1b657c8fd366b9825
Nicole Stellatos <n_stellatos@yahoo.com>
2019-02-20 21:13:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var greeting = "Hello, everybody!";
+// variables store data
 
+var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson"
+// variables are assigned
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
-
+// combined strings - concatenation
 var topic = "space";
 
-var conversation = `Let\'s talk about ${topic}.`;
+var conversation = `Let's talk about ${topic}.`;
+
+//${} will insert variable into the string
